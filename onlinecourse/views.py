20cd0e557b15706620cbd08464f02f51bd65b8f9
Rajesh Jain <rajesjai@gmail.com>
2021-08-02 08:24:57 +0000

    Some more changes

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-# <HINT> Import any new Models here
+from .models import Course, Enrollment, Question, Choice, Submission
 from .models import Course, Enrollment
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
@@ -131,6 +131,63 @@ def enroll(request, course_id):
         # For each selected choice, check if it is a correct answer or not
         # Calculate the total score
 #def show_exam_result(request, course_id, submission_id):
+def submit(request, course_id):
+    course = get_object_or_404(Course, pk=course_id)
+    user = request.user
+    enrollment = Enrollment.objects.filter(user=user, course=course).get()
 
+    submission = Submission.objects.create(enrollment_id = enrollment.id )
 
+    answers =  extract_answers(request)
+    for a in answers:
+        temp_c = Choice.objects.filter(id = int(a)).get()
+        submission.choices.add(temp_c)
 
+    submission.save()         
+    return HttpResponseRedirect(reverse(viewname='onlinecourse:show_exam_result', args=(course.id,submission.id ))) 
+
+def extract_answers(request):
+    submitted_anwsers = []
+    for key in request.POST:
+        if key.startswith('choice'):
+            value = request.POST[key]
+            choice_id = int(value)
+            submitted_anwsers.append(choice_id)
+    return submitted_anwsers
+
+def show_exam_result(request, course_id, submission_id):
+    course  =  get_object_or_404(Course, pk=course_id)
+    submission =      get_object_or_404(Submission, pk=submission_id)
+    total =  0
+    total_user =  0
+    q_results = {}
+    c_submits = {}
+    c_results = {}
+    for q in course.question_set.all():
+        q_total = 0;
+        q_total_user = 0;
+        for c in q.choice_set.all():
+            q_total += 1  
+            temp_right = c.is_correct
+            count =  submission.choices.filter(id = c.id).count()
+
+            temp_user  = count > 0 
+            c_submits[c.id] = temp_user
+            c_results[c.id] = temp_user == temp_right
+            if temp_user == temp_right:
+                q_total_user += 1        
+        q_results[q.id] =  q.grade*(q_total_user / q_total)
+        total += q.grade 
+        total_user  += q_results[q.id]
+    context  = {}
+    context["course"]  =  course
+    context["submission"]  =  submission
+
+    context["total"]  =  total
+    context["total_user"]  =  total_user
+    context["q_results"]  =  q_results
+    context["c_submits"]  =  c_submits
+    context["c_results"]  =  c_results
+    context["grade"]  =  int((total_user/total)*100)
+
+    return render(request, 'onlinecourse/exam_result_bootstrap.html', context)
